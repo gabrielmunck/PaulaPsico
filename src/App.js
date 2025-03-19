9ac51f0c75d3bca3e217a97a5bcf33493efeece0
Gabriel Munck <munckgabriel@gmail.com>
2025-03-19 15:21:59 -0300

    Created Publi section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Herocard from "./components/Herocard";
+import Publi from "./components/Publi";
 
 function App() {
   return (
-    <div className="min-h-screen bg-background flex items-center justify-center">
+    <div className="min-h-screen bg-background flex flex-col items-center justify-center">
       <Herocard />
+      <Publi />
     </div>
   );
 }
---
+++ src/components/Publi.jsx
@@ -0,0 +1,8 @@
+function Publi() {
+  return (
+    <div className="w-full h-1/2 bg-white flex flex-col items-center justify-center">
+      <h1>Publi</h1>
+    </div>
+  );
+}
+export default Publi;
