00b40544972b7c35e5c2b283eacfc93ac53a6ba6
Gabriel Munck <munckgabriel@gmail.com>
2025-03-20 03:49:02 -0300

    Created Publi section

--- src/components/Herocard.jsx
+++ src/components/Herocard.jsx
@@ -4,8 +4,8 @@ import paulaPsico from "../assets/PaulaPsico.png";
 function Herocard() {
   return (
     <div className="">
-      <div className="flex flex-col md:flex-row items-center gap-4 px-12 md:px-32 py-10">
-        {/* Text container */}
+      <div className="flex flex-col md:flex-row items-center gap-4 px-12 md:px-32 py-10 z-20">
+        
         <div className="w-full md:w-1/2 mt-4 md:mt-0">
           <h2 className="text-2xl font-bold text-main mb-3">Paula -</h2>
           <p className="font-lato font-bold tracking-wider text-xl md:text-5xl md:px-10 text-brown-text text-center">
@@ -13,12 +13,12 @@ function Herocard() {
             uma vida com propósito e bem-estar.”
           </p>
           <div className="flex justify-center mt-2">
-            <button className="w-2/3 md:w-1/2 mt-4 bg-main text-white px-6 py-2 rounded-md hover:bg-opacity-90 transition-all">
+            <button className="w-2/3 md:w-1/2 mt-4 bg-main text-white shadow-md px-6 py-2 rounded-md hover:bg-opacity-90 transition-all">
               Saiba mais
             </button>
           </div>
         </div>
-        {/* Image container - circular image */}
+        
         <div className="w-full md:w-1/2 flex justify-center">
           <div className="w-48 h-48 md:w-[500px] md:h-[500px] overflow-hidden rounded-full border-2 border-main">
             <img
--- src/components/Publi.jsx
+++ src/components/Publi.jsx
@@ -1,8 +1,45 @@
+import tree from "../assets/tree.svg";
+import whatsapp from "../assets/whatsapp.svg";
+
 function Publi() {
   return (
-    <div className="w-full h-1/2 bg-white flex flex-col items-center justify-center">
-      <h1>Publi</h1>
+    <div className="relative w-full py-16 flex flex-col items-center justify-center overflow-y-visible">
+      <div className="absolute left-0 top-1/3 h-[400px] md:h-[600px] lg:h-[800px] w-full z-0">
+        <img
+          src={tree}
+          alt="Árvore"
+          className="absolute -left-[180px] md:-left-[250px] top-10 md:top-0 transform -translate-y-1/2 w-auto h-full opacity-80"
+        />
+      </div>
+
+      <div className="w-4/5 md:3/5 max-w-5xl z-10">
+        <div className="flex flex-col justify-center items-center w-full rounded-[80px] border-4 border-main p-8 mb-6">
+          <p className="text-contrast font-lato font-bold text-xl mb-8">
+            Sua saúde mental é tão importante quanto a sua saúde física.
+          </p>
+          <p className="text-contrast font-lato font-extralight text-lg mb-6">
+            Tenha sua consulta marcada em apenas alguns cliques{" "}
+          </p>
+          <div className="flex p-8 mb-6">
+            <button className="flex items-center bg-emerald-500 px-10 py-5 rounded-full hover:bg-emerald-700 transition-colors">
+              <img
+                src={whatsapp}
+                alt="whatsapp icon"
+                className="w-8 h-8 mr-4"
+              />
+              <p className="text-white font-lato font-bold">AGENDE UMA CONSULTA</p>
+            </button>
+          </div>
+          <p className="text-contrast tracking-wide font-lato font-bold text-lg mb-8">
+            CONSULTAS PRESENCIAIS E ONLINE{" "}
+          </p>
+          <p className="text-contrast tracking-wide font-lato font-extralight text-lg mb-8">
+            Atendimento para crianças, adolescentes, adultos e casal{" "}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
+
 export default Publi;
