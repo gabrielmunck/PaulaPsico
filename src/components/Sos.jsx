266ce2f258948e1fa585f3eb25b23dc9e2af1000
Gabriel Munck <munckgabriel@gmail.com>
2025-04-23 13:32:32 -0300

    Minor fixes

--- src/components/Sos.jsx
+++ src/components/Sos.jsx
@@ -2,7 +2,7 @@ function Sos() {
   return (
     <footer>
       <div className="w-full bg-background flex flex-col items-center justify-center py-10">
-        <div className="w-full flex flex-col items-center justify-center mb-10 px-6 ">
+        <div className="w-full flex flex-col items-center justify-center px-6 ">
           <h4 className="flex flex-col md:flex-row justify-center w-full font-lato font-light text-center text-lg md:text-xl mb-8">
             <p className="font-bold mr-2">Atenção:</p> Este site não oferece
             atendimento imediato a pessoas em crise suicida.
@@ -10,7 +10,7 @@ function Sos() {
           <h3 className="w-full font-lato font-bold text-center text-xl md:text-2xl mb-8 tracking-wider">
             Em caso de crise ligue para o CVV - 188
           </h3>
-          <p className="w-full font-lato font-light text-center text-lg md:text-xl mb-8 md:px-10">
+          <p className="w-full font-lato font-light text-center text-lg md:text-xl md:px-10">
             Em caso de emergência, procure o hospital mais próximo. Havendo
             risco de morte, ligue imediatamente para o SAMU (192) ou Corpo de
             Bombeiro (193).
