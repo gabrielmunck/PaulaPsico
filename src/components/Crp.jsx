3805bdf9b7be208c1f2e2a8b14bb59b563802289
Gabriel Munck <munckgabriel@gmail.com>
2025-04-24 00:36:57 -0300

    Updated info

--- src/components/Crp.jsx
+++ src/components/Crp.jsx
@@ -4,7 +4,7 @@ function Crp() {
   return (
     <div className="w-full h-24 bg-contrast">
         <div className=" w-full h-full flex justify-center items-center p-2">
-            <h3 className="flex max-w-[1400px] justify-center items-center font-lato text-md md:text-lg text-center text-white tracking-wider">Paula Mie Buno Hiratsuka - Psicólogo Responsável - (CRP 06/178828)</h3>
+            <h3 className="flex max-w-[1400px] justify-center items-center font-lato text-md md:text-lg text-center text-white tracking-wider">Paula Mie Buno Hiratsuka - Psicólogo Responsável - (CRP 06/182982)</h3>
         </div>
     </div>
   )
